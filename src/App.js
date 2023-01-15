1f92226d371d6c72c762d44c8f030b1aee58a9ad
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-01-14 22:46:50 -0700

    hero section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Layout from './components/Layout'
+import Main from './components/Home'
+
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 
@@ -16,10 +18,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            {/* <Route index element={<Main />} />
-            <Route path='/about' element={<About />} />
+            <Route index element={<Main />} />
+            {/* <Route path='/about' element={<About />} />
             <Route path='/contact' element={<Contact />} />
-            <Route path='/portfolio' element={<Portfolio />} /> */}
+            <Route path='/portfolio' element={<Portfolio />} /> */} 
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="container home-page">
+        <h1>Muslim</h1>
+        <h1>Mental</h1>
+        <h1>Wealth</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
